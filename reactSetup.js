34d61f43630ae9a5673465a32b4b8b3406a37156
AKRISHT <89027645+akrisht2805@users.noreply.github.com>
2022-08-29 16:02:48 +0530

    setup complete

---
+++ reactSetup.js
@@ -0,0 +1,6 @@
+Step 1 : Download Node JS and npm 
+Step 2 : Intall any text-editor
+Step 3 : Using node js install the create-node-app package.
+          install -g create-react-app
+Step 4 : Create react app
+          create-react-app myapp
