a75b42cd5dd34f4fc85943f28005e8cb71596b94
AKRISHT <89027645+akrisht2805@users.noreply.github.com>
2022-08-29 15:48:59 +0530

    Introduction Complete

---
+++ Intoduction.js
@@ -0,0 +1,22 @@
+WHAT IS REACTJS :
+  Javascript Library 
+  Use to Create UI
+  Open Source
+  Developed And Managed By Facebook
+  
+FEATURES OF REACT :
+  JSX (Html + Js)
+  Components
+  One Way data binding (source -> data)
+  Virtual DOM
+  Simplicity
+  Performance (Light and Fast)
+
+RENDERING DOM :
+  Actual DOM : document.getElementById("...");
+  Virutal DOM : The virtual DOM (VDOM) is a programming concept where an ideal, or “virtual”, representation of a UI is kept in memory and synced with the “real” DOM by a library such as ReactDOM. This process is called reconciliation.
+  
+
+
+  
+  
